b492d975b3e77ee1b6e7a6129398374c49dd8834
Ethan Frigon <ethan.frigon@gmail.com>
2018-09-26 09:13:53 -0700

    added creator

---
+++ creator-copy.js
@@ -0,0 +1,22 @@
+'use strict';
+// In this drill, you need to write code for the createMyObject function so that it returns an object with the following key value pairs:
+
+// foo => 'bar'
+// answerToUniverse => 42
+// olly olly => 'oxen free'
+// sayHello => a function that returns the string 'hello'
+
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'Hello';
+    }
+  }
+}
+let object = createMyObject();
+console.log(createMyObject());
+console.log(object.foo);
+console.log(object.sayHello());
