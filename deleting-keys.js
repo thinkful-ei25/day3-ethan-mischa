c153a1b052d2db97918ca9b991f20b3500f8fce0
mischb <mischarberlin@gmail.com>
2018-09-26 12:35:07 -0400

    added deleting key.js

---
+++ deleting-keys.js
@@ -0,0 +1,15 @@
+'use strict';
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+}
+console.log(sampleObj);
+keyDeleter(sampleObj);
+console.log(sampleObj);
